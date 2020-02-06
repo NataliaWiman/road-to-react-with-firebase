66508a09808494442b7195c59db8c8274370bbca
NataliaWiman <natalia.wiman@gmail.com>
2020-02-06 20:18:32 +0100

    Application setup

---
+++ src/components/PasswordChange/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PasswordChange = () => {
+  return <div></div>;
+};
---
+++ src/components/Session/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Session = () => {
+  return <div></div>;
+};
