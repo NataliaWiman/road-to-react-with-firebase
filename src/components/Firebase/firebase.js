ff8ccd1a7f4057dff74c917d2ec2d72837f99adf
NataliaWiman <natalia.wiman@gmail.com>
2020-02-06 21:55:47 +0100

    Set up Authentication API

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from "firebase/app";
+import "firebase/auth";
 
 var config = {
   apiKey: "AIzaSyCIJZDwu0aGjzUNbJwFk2n07hj-HnBtQrw",
@@ -12,7 +13,21 @@ var config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
+    this.auth = app.auth();
   }
+
+  // *** Auth API ***
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
